2cbb716b4cb33928ab19c48cbdcd7efa1c3abfa2
flexphere <ankh@flexphere.com>
2020-03-30 01:56:38 +0900

    omit unused functions

--- src/lib/Speecher.ts
+++ src/lib/Speecher.ts
@@ -10,38 +10,6 @@ export class Speecher extends Base {
     userId: string = "";
     connection!: Discord.VoiceConnection;
 
-    @Command('!speecher connect')
-    async Connect(message: Discord.Message, ...args: string[]) {
-        if (message.author.bot) {
-            return;
-        }
-
-        if ( ! (message.channel instanceof Discord.TextChannel)) {
-            return;
-        }
-
-        const member = message.member;
-        if ( ! member) {
-            return this.flashMessage(message.channel, `｡ﾟ(ﾟ´Д｀ﾟ)ﾟ｡あんた誰・・`);
-        }
-
-        if ( ! member.voice.channel) {
-            return this.flashMessage(message.channel, `｡ﾟ(ﾟ´Д｀ﾟ)ﾟ｡音声チャンネルに入ってからやってくれい`);
-        }
-
-        this.userId = message.author.id;
-        this.connection = await member.voice.channel?.join();
-        if ( ! this.connection) {
-            return;
-        }
-    }
-
-    @Command('!speecher disconnect')
-    async Disconnect(message: Discord.Message, ...args: string[]) {
-        this.userId = '';
-        this.connection.disconnect();
-    }
-
     @Listen('message')
     async Speak(message: Discord.Message, ...args: string[]) {
         try {
