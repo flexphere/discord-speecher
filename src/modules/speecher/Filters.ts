bf9c441e18806e0ffe3e0d081389080672813b41
ankh <ankh@flexphere.com>
2021-09-22 14:41:42 +0900

    fix for the fix

--- src/modules/speecher/Filters.ts
+++ src/modules/speecher/Filters.ts
@@ -17,6 +17,5 @@ export const removeURL = (message: string) => {
 }
 
 export const emojiToLabel = (message: string) => {
-  const emojiRegex = new RegExp('(.*?)(<a|a)*:(.*?):.*','g');
-  return message.match(emojiRegex)?.map(m=>m.split(' ').map(s=>s.replace(emojiRegex,'$1 $3')).join(' ')).join() || message;
+  return message.replace(/<([^\d]+)\d+>/g, "$1").replace(/<a:(.*):\d+>/g, '$1');
 }
